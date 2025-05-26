84c6aebca7d5c4cf39c36d94ab95a98e3355e6a6
Rodrigo Ferreira <rodrigo.ferreiradaslv@gmail.com>
2025-05-26 14:32:55 -0300

    feat(User-Service): Infrastructure setup and add UserDbContext

---
+++ src/Modules/Users/TechMesh.User.Infrastructure/Context/UserDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechMesh.User.Domain.Entities;
+
+namespace TechMesh.User.Infrastructure.Context;
+
+public class UserDbContext : DbContext
+{
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Domain.Entities.User> Users { get; set; }
+    public DbSet<Technology> Technologies { get; set; }
+}
