05a46f0008cc52989f558f80d9551257d4aa7b45
Rodrigo Ferreira <rodrigo.ferreiradaslv@gmail.com>
2025-06-10 21:32:23 -0300

    feat(User-Service): update database migration for timestamp fields

---
+++ src/Modules/Users/TechMesh.User.Infrastructure/Migrations/20250610182641_Change_Timestamptz_To_Timestamp.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TechMesh.User.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Change_Timestamptz_To_Timestamp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "user",
+                table: "users",
+                type: "TIMESTAMP",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "user",
+                table: "users",
+                type: "TIMESTAMP",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "user",
+                table: "technologies",
+                type: "TIMESTAMP",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "user",
+                table: "technologies",
+                type: "TIMESTAMP",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "user",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "user",
+                table: "users",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                schema: "user",
+                table: "technologies",
+                type: "timestamp with time zone",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                schema: "user",
+                table: "technologies",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "TIMESTAMP");
+        }
+    }
+}
